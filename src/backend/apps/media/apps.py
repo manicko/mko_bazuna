fb7f5f070642d187cd8709fbb57f5e7d2e0938d2
manicko <manicko@yandex.ru>
2026-07-28 15:12:54 +0200

    feat(media): create apps/media app module structure
    Task: TASK_010_Create_media_app_structure

---
+++ src/backend/apps/media/apps.py
@@ -0,0 +1,10 @@
+"""
+Media app for photo thumbnail generation and media processing.
+"""
+
+from django.apps import AppConfig
+
+
+class MediaConfig(AppConfig):
+    name = "apps.media"
+    verbose_name = "Media"
